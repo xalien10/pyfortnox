a0ecbd988c1c638facef2d75b43d27a122ccfb6b
Mahmudul Hasan <ikhtiar@Mahmuduls-MacBook-Air.local>
2020-06-15 20:49:01 +0600

    voucher creation with optional fields enabled

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfortnox",
-    version="1.1.1",
+    version="1.1.2",
     author="Mahmudul Hasan",
     author_email="ikhtiarcse10ruet@gmail.com",
     description="Fortnox API V3 library client for Python",
